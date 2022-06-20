fe42263ba4a9c30d819ae8c5dac952657adaf73d
Renato Piacente <97764409+RenatoPiacente@users.noreply.github.com>
2022-06-20 10:59:51 -0400

    created items.csv
    added a csv file to import items names, prices and quantity to update our program

--- class_1.py
+++ class_1.py
@@ -1,26 +1,56 @@
+import csv
+
+
 class Item:
-    discount_rate = 0.8 # The pay rate after 20% discount.
+    discount_rate = 0.8  # Applies 20% discount to the final price of a product.
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
-        assert price >=0, f'Price {price} is not greater or equal to zero!'
-        assert quantity >=0, f'Quantity {quantity} is not greater or equal to zero!'
+        assert price >= 0, f'Price {price} is not greater or equal to zero!'
+        assert quantity >= 0, f'Quantity {quantity} is not greater or equal to zero!'
         
         # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+        
+        # Actions to be executed
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
-    
 
     def apply_discount(self):
         self.price *= self.discount_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as file:
+            reader = csv.DictReader(file)
+            items = list(reader)
 
-item_1 = Item('Phone', 100, 1)
-item_2 = Item('Laptop', 1000, 3)
-item_3 = Item('Cabe', 10, 5)
-item_4 = Item('Mouse', 50, 5)
-item_5 = Item('Keyboard', 75, 5)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+
+    @staticmethod
+    def is_integer(num):
+        """
+        Will count out the floats that are equal to zero.
+        : param num: will get a number and anilyze if it's float point is == 0
+        : return: True if a number is an integer and False if not
+        """
+        if isinstance(num, float):
+            # Counts out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f'Item("{self.name}, {self.price}, {self.quantity}")'
